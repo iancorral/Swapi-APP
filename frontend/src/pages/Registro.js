9f9d199a01ff7c85395d777d23a307c466556acf
Omar Acuña <13097@ulsachihuahua.edu.mx>
2025-06-05 21:58:44 -0600

    .

--- frontend/src/pages/Registro.js
+++ frontend/src/pages/Registro.js
@@ -33,13 +33,15 @@ function Register() {
 
   return (
     <div className="page-container">
-      <h2>Registro</h2>
-      <form className="form-box" onSubmit={handleRegister}>
-        <input type="text" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
-        <input type="email" placeholder="Correo institucional" value={correo} onChange={(e) => setCorreo(e.target.value)} required />
-        <input type="password" placeholder="Contraseña" value={contrasena} onChange={(e) => setContrasena(e.target.value)} required />
-        <button type="submit">Registrarse</button>
-      </form>
+      <div className="boxed-container">
+        <h2>Registro</h2>
+        <form className="form-box" onSubmit={handleRegister}>
+          <input type="text" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
+          <input type="email" placeholder="Correo institucional" value={correo} onChange={(e) => setCorreo(e.target.value)} required />
+          <input type="password" placeholder="Contraseña" value={contrasena} onChange={(e) => setContrasena(e.target.value)} required />
+          <button type="submit">Registrarse</button>
+        </form>
+      </div>
     </div>
   );
 }
