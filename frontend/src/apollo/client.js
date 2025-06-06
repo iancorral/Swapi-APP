1499b5cd872c5839cd42bb784320324f0ac82d65
Ian Corral <12130@ulsachihuahua.edu.mx>
2025-06-05 19:48:07 -0600

    Se agrega Link a base de datos por medio de Render

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/', // o el endpoint en producción
+  uri: 'https://swapi-backend.onrender.com', // o el endpoint en producción
 });
 
 const client = new ApolloClient({
